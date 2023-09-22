db2a8a4a651cca48738bf56cac4ef30bcbde2bd6
Sam <145726056+CyberCenturion23@users.noreply.github.com>
2023-09-22 11:37:26 -0400

    Update TAXCALCUS.js
    Added the states: South Carolina, North Carolina, Alabama, and Mississippi.

--- TAXCALCUS.js
+++ TAXCALCUS.js
@@ -3,10 +3,38 @@ println("COUNTIES");
 println("1: Florida");
 println("2: Georgia");
 println("3: Tennesee");
+println("4: South Carolina);
+println("5: North Carolina);
+println("6: Alabama);
+println("7: Mississippi);
 var tax = 0
 var state = readInt("Please enter your state number: ");
 if (state = 1){
     println("You have selected: Florida.");
+    var flcounty = 0
+    if ()
+} else if (state = 2){
+    println("You have selected: Georgia.");
+    var flcounty = 0
+    if ()
+} else if (state = 3){
+    println("You have selected: Tennessee.");
+    var flcounty = 0
+    if ()
+} else if (state = 4){
+    println("You have selected: South Carolina.");
+    var flcounty = 0
+    if ()
+} else if (state = 5){
+    println("You have selected: North Carolina.");
+    var flcounty = 0
+    if ()
+} else if (state = 6){
+    println("You have selected: Alabama.");
+    var flcounty = 0
+    if ()
+} else if (state = 7){
+    println("You have selected: Mississippi.");
     var flcounty = 0
     if ()
 }
