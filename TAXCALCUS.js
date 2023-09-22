cba029e4cd5f63ea875cc3bfe28a40f5b76b22dd
PyrosStuff <119268845+PyrosStuff@users.noreply.github.com>
2023-09-22 11:29:27 -0400

    Update TAXCALCUS.js

---
+++ TAXCALCUS.js
@@ -0,0 +1,12 @@
+println("Welcome to the US sales tax calculator!");
+println("COUNTIES");
+println("1: Florida");
+println("2: Georgia");
+println("3: Tennesee");
+var tax = 0
+var state = readInt("Please enter your state number: ");
+if (state = 1){
+    println("You have selected: Florida.");
+    var
+    if ()
+}
