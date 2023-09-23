f63157ac0dff5842c3780b2d0c6845d4009b2429
PyrosStuff <119268845+PyrosStuff@users.noreply.github.com>
2023-09-22 19:44:28 -0700

    Update TAXCALCUS.js

--- TAXCALCUS.js
+++ TAXCALCUS.js
@@ -8,33 +8,34 @@ println("5: North Carolina");
 println("6: Alabama");
 println("7: Mississippi");
 var tax = 0
-var state = readInt("Please enter your state number: ");
+var state = 0
+state = readInt("Please enter your state number: ");
 if (state = 1){
     println("You have selected: Florida.");
     var flcounty = 0
-    flcounty = readInt("Select your county.");
+    flcounty = readInt("Select your county. ");
 } else if (state = 2){
     println("You have selected: Georgia.");
     var gacounty = 0
-    gacounty = readInt("Select your county.");
+    gacounty = readInt("Select your county. ");
 } else if (state = 3){
     println("You have selected: Tennessee.");
     var tncounty = 0
-    tncounty = readInt("Select your county.");
+    tncounty = readInt("Select your county. ");
 } else if (state = 4){
     println("You have selected: South Carolina.");
     var sccounty = 0
-    sccounty = readInt("Select your county.");
+    sccounty = readInt("Select your county. ");
 } else if (state = 5){
     println("You have selected: North Carolina.");
     var nccounty = 0
-    nccounty = readInt("Select your county.");
+    nccounty = readInt("Select your county. ");
 } else if (state = 6){
     println("You have selected: Alabama.");
     var alcounty = 0
-    alcounth = readInt("Select your county.");
+    alcounty = readInt("Select your county. ");
 } else if (state = 7){
     println("You have selected: Mississippi.");
     var mscounty = 0
-    mscounty = readInt("Select your county.");
+    mscounty = readInt("Select your county. ");
 }
