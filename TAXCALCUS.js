9c7dfa3955fc3e082a1806779d27f97c08d4b8b9
Sam <145726056+CyberCenturion23@users.noreply.github.com>
2023-09-23 11:48:58 -0400

    Update TAXCALCUS.js

--- TAXCALCUS.js
+++ TAXCALCUS.js
@@ -10,31 +10,31 @@ println("7: Mississippi");
 var tax = 0
 var state = 0
 state = readInt("Please enter your state number: ");
-if (state = 1){
+if (state == 1){
     println("You have selected: Florida.");
     var flcounty = 0
     flcounty = readInt("Select your county. ");
-} else if (state = 2){
+} else if (state == 2){
     println("You have selected: Georgia.");
     var gacounty = 0
     gacounty = readInt("Select your county. ");
-} else if (state = 3){
+} else if (state == 3){
     println("You have selected: Tennessee.");
     var tncounty = 0
     tncounty = readInt("Select your county. ");
-} else if (state = 4){
+} else if (state == 4){
     println("You have selected: South Carolina.");
     var sccounty = 0
     sccounty = readInt("Select your county. ");
-} else if (state = 5){
+} else if (state == 5){
     println("You have selected: North Carolina.");
     var nccounty = 0
     nccounty = readInt("Select your county. ");
-} else if (state = 6){
+} else if (state == 6){
     println("You have selected: Alabama.");
     var alcounty = 0
     alcounty = readInt("Select your county. ");
-} else if (state = 7){
+} else if (state == 7){
     println("You have selected: Mississippi.");
     var mscounty = 0
     mscounty = readInt("Select your county. ");
